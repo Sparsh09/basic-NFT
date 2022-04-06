3a0ce6eaf1076aba92cb07af495fecc6e3869611
Sparsh09 <sparsh1698.cse18@chitkara.edu.in>
2022-04-07 00:17:01 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -10,7 +10,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-require("dotenv").config();
+
 
 module.exports = {
   solidity: "0.8.4",
